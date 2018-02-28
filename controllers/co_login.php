e2cbbda220de3da5c5cbe270e0993426d1b87774
Chithen <Chithen@DESKTOP-H2N5CGD>
2018-02-28 13:08:48 +0530

    get user role from db

--- controllers/co_login.php
+++ controllers/co_login.php
@@ -29,7 +29,7 @@ if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
 
         session_start();
-        
+
         $_SESSION["u_id"] = $row["iduser"];
         $_SESSION["u_fname"] = $row["user_fname"];
         $_SESSION["u_lname"] = $row["user_lname"];
@@ -39,13 +39,25 @@ if (mysqli_num_rows($result) > 0) {
         $_SESSION["u_email"] = $row["user_email"];
         $_SESSION["u_state"] = $row["user_state"];
 
+        $sqlForGetRoll = "SELECT * FROM role WHERE idrole='" . $row["role_idrole"] . "'";
+
+        $resultForRoll = mysqli_query($connection, $sqlForGetRoll);
+
+        if (mysqli_num_rows($resultForRoll) > 0) {
+
+            while ($row1 = mysqli_fetch_assoc($resultForRoll)) {
+                
+                $_SESSION["u_roll"] = $row1["role_name"];
+                
+                
+            }
+        }
+
         echo 'ok';
- 
-    } 
+    }
 } else {
 
     echo 'Invalide Username Or Password';
-    
 }
 
 
